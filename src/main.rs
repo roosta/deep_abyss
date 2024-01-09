527b9a375ff44084ec3a5460ad3735fba812a72d
Daniel Berg <mail@roosta.sh>
2024-01-09 17:16:07 +0100

    Setup basic movement

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,60 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .run();
+const PLAYER_SPEED: f32 = 400.0;
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+#[derive(Component, Deref, DerefMut)]
+struct Direction(Vec2);
+
+#[derive(Component)]
+struct Player;
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity, &Direction)>, time: Res<Time>) {
+    for (mut transform, velocity, direction) in &mut query {
+        transform.translation.x += velocity.x * direction.x * time.delta_seconds();
+        transform.translation.y += velocity.y * direction.y * time.delta_seconds();
+    }
+}
+
+fn handle_input(
+    keys: Res<Input<KeyCode>>,
+    mut query: Query<&mut Direction>
+) {
+    let mut direction = query.single_mut();
+    direction.x = 0.;
+    direction.y = 0.;
+    if keys.pressed(KeyCode::Left) {
+        direction.x = -1.;
+    }
+    if keys.pressed(KeyCode::Right) {
+        direction.x = 1.;
+    }
+    if keys.pressed(KeyCode::Up) {
+        direction.y = 1.;
+    }
+    if keys.pressed(KeyCode::Down) {
+        direction.y = -1.;
+    }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
+    commands.spawn((SpriteBundle {
         texture: asset_server.load("player/placeholder.png"),
         ..default()
-    });
+    },
+    Player,
+    Velocity(Vec2::new(PLAYER_SPEED, PLAYER_SPEED)),
+    Direction(Vec2::new(0., 0.))
+    ));
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (handle_input, apply_velocity).chain())
+        .run();
 }
