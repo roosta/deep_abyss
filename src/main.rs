c2cfae97af00095c9bffd513d32581a489b10a60
Daniel Berg <mail@roosta.sh>
2024-01-15 21:53:38 +0100

    feat: render collision grid
    Render rectangles as proof of concept, wiring in collisions next.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_ecs_ldtk::prelude::*;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+
+// use std::collections::HashSet;
+
+#[derive(Default, Component)]
+struct Collider;
 
 const PLAYER_SPEED: f32 = 200.0;
-const PLAYER_SIZE: f32 = 32.;
-const GRID_SIZE: i32 = 16;
+// const PLAYER_SIZE: f32 = 32.;
+// const GRID_SIZE: i32 = 16;
+
+#[derive(Event, Default)]
+struct CollisionEvent;
 
 #[derive(Default, Component, Deref, DerefMut)]
 struct Velocity(Vec2);
@@ -16,13 +27,13 @@ struct Direction(Vec2);
 #[derive(Default, Component)]
 struct Player;
 
-
 #[derive(Default, Component)]
 struct Wall;
 
-#[derive(Default, Bundle, LdtkIntCell)]
+#[derive(Bundle, Default, LdtkIntCell)]
 struct WallBundle {
     wall: Wall,
+    collider: Collider
 }
 
 // #[derive(Default, Resource)]
@@ -37,8 +48,8 @@ struct PlayerBundle {
     player: Player,
     #[with(init_velocity)]
     velocity: Velocity,
-    // #[with(init_direction)]
     direction: Direction,
+    collision: Collider,
     #[sprite_sheet_bundle]
     sprite_bundle: SpriteSheetBundle,
 }
@@ -58,6 +69,161 @@ fn apply_velocity(
 
         transform.translation.x = pos_x;
         transform.translation.y = pos_y;
+    }
+}
+
+fn check_collisions(
+    mut commands: Commands,
+    mut collision_events: EventWriter<CollisionEvent>,
+    wall_query: Query<(&GridCoords, &Parent), Added<Wall>>,
+    parent_query: Query<&Parent, Without<Wall>>,
+    level_query: Query<(Entity, &LevelIid)>,
+    ldtk_projects: Query<&Handle<LdtkProject>>,
+    ldtk_project_assets: Res<Assets<LdtkProject>>,
+    // mut player_query: Query<(&mut Velocity, &Transform), With<Player>>,
+    // collider_query: Query<(Entity, &Transform, &Wall), With<Collider>>,
+) {
+
+    /// Represents a wide wall that is 1 tile tall
+    /// Used to spawn wall collisions
+    #[derive(Clone, Eq, PartialEq, Debug, Default, Hash)]
+    struct Plate {
+        left: i32,
+        right: i32,
+    }
+
+    /// A simple rectangle type representing a wall of any size
+    struct Rect {
+        left: i32,
+        right: i32,
+        top: i32,
+        bottom: i32,
+    }
+
+    // Consider where the walls are
+    // storing them as GridCoords in a HashSet for quick, easy lookup
+    //
+    // The key of this map will be the entity of the level the wall belongs to.
+    // This has two consequences in the resulting collision entities:
+    // 1. it forces the walls to be split along level boundaries
+    // 2. it lets us easily add the collision entities as children of the appropriate level entity
+    let mut level_to_wall_locations: HashMap<Entity, HashSet<GridCoords>> = HashMap::new();
+
+    wall_query.for_each(|(&grid_coords, parent)| {
+
+        if let Ok(grandparent) = parent_query.get(parent.get()) {
+            level_to_wall_locations
+                .entry(grandparent.get())
+                .or_default()
+                .insert(grid_coords);
+        }
+    });
+    if !wall_query.is_empty() {
+        level_query.for_each(|(level_entity, level_iid)| {
+            if let Some(level_walls) = level_to_wall_locations.get(&level_entity) {
+                let ldtk_project = ldtk_project_assets
+                    .get(ldtk_projects.single())
+                    .expect("Project should be loaded if level has spawned");
+
+                let level = ldtk_project
+                    .as_standalone()
+                    .get_loaded_level_by_iid(&level_iid.to_string())
+                    .expect("Spawned level should exist in LDtk project");
+
+                let LayerInstance {
+                    c_wid: width,
+                    c_hei: height,
+                    grid_size,
+                    ..
+                } = level.layer_instances()[0];
+
+                // combine wall tiles into flat "plates" in each individual row
+                let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
+
+                for y in 0..height {
+                    let mut row_plates: Vec<Plate> = Vec::new();
+                    let mut plate_start = None;
+
+                    // + 1 to the width so the algorithm "terminates" plates that touch the right edge
+                    for x in 0..width + 1 {
+                        match (plate_start, level_walls.contains(&GridCoords { x, y })) {
+                            (Some(s), false) => {
+                                row_plates.push(Plate {
+                                    left: s,
+                                    right: x - 1,
+                                });
+                                plate_start = None;
+                            }
+                            (None, true) => plate_start = Some(x),
+                            _ => (),
+                        }
+                    }
+
+                    plate_stack.push(row_plates);
+                }
+
+                // combine "plates" into rectangles across multiple rows
+                let mut rect_builder: HashMap<Plate, Rect> = HashMap::new();
+                let mut prev_row: Vec<Plate> = Vec::new();
+                let mut wall_rects: Vec<Rect> = Vec::new();
+
+                // an extra empty row so the algorithm "finishes" the rects that touch the top edge
+                plate_stack.push(Vec::new());
+
+                for (y, current_row) in plate_stack.into_iter().enumerate() {
+                    for prev_plate in &prev_row {
+                        if !current_row.contains(prev_plate) {
+                            // remove the finished rect so that the same plate in the future starts a new rect
+                            if let Some(rect) = rect_builder.remove(prev_plate) {
+                                wall_rects.push(rect);
+                            }
+                        }
+                    }
+                    for plate in &current_row {
+                        rect_builder
+                            .entry(plate.clone())
+                            .and_modify(|e| e.top += 1)
+                            .or_insert(Rect {
+                                bottom: y as i32,
+                                top: y as i32,
+                                left: plate.left,
+                                right: plate.right,
+                            });
+                    }
+                    prev_row = current_row;
+                }
+
+                commands.entity(level_entity).with_children(|level| {
+                    // Spawn colliders for every rectangle..
+                    // Making the collider a child of the level serves two purposes:
+                    // 1. Adjusts the transforms to be relative to the level for free
+                    // 2. the colliders will be despawned automatically when levels unload
+                    for wall_rect in wall_rects {
+                        level.spawn(SpriteBundle {
+                                sprite: Sprite {
+                                    color: Color::rgb(0.25, 0.25, 0.75),
+                                    custom_size: Some(Vec2::new(
+                                            (wall_rect.right as f32 - wall_rect.left as f32 + 1.)
+                                            * grid_size as f32,
+                                            (wall_rect.top as f32 - wall_rect.bottom as f32 + 1.)
+                                            * grid_size as f32,
+                                            )),
+                                        ..default()
+                                },
+                                transform: Transform::from_xyz(
+                                    (wall_rect.left + wall_rect.right + 1) as f32 * grid_size as f32
+                                    / 2.,
+                                    (wall_rect.bottom + wall_rect.top + 1) as f32 * grid_size as f32
+                                    / 2.,
+                                    50.,
+                                    ),
+                                ..default()
+                            }
+                        );
+                    }
+                });
+            }
+        });
     }
 }
 
@@ -101,11 +267,13 @@ fn main() {
         .add_plugins(LdtkPlugin)
         .insert_resource(LevelSelection::index(0))
         .add_plugins(WorldInspectorPlugin::new())
+        .add_event::<CollisionEvent>()
         .add_systems(Startup, setup)
         .register_ldtk_entity::<PlayerBundle>("Player")
-        // .register_ldtk_int_cell::<WallBundle>(1)
+        .register_ldtk_int_cell::<WallBundle>(1)
         // .init_resource::<LevelWalls>()
-        .add_systems(Update, (handle_input, apply_velocity).chain())
+        .add_systems(Update,
+            (handle_input, check_collisions, apply_velocity).chain())
         .run();
 }
 
