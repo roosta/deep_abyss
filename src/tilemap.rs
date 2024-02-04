29bd72fb6056ceb5a806f3f78fdaf6f9af5f9cd2
Daniel Berg <mail@roosta.sh>
2024-02-04 12:38:41 +0100

    docs: update spawn_collisions docstring
    Added the original comment + src + modified note

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -50,6 +50,27 @@ pub struct TilemapPlugin;
 // }
 
 
+/// Spawns heron collisions for the walls of a level
+///
+/// You could just insert a ColliderBundle in to the WallBundle,
+/// but this spawns a different collider for EVERY wall tile.
+/// This approach leads to bad performance.
+///
+/// Instead, by flagging the wall tiles and spawning the collisions later,
+/// we can minimize the amount of colliding entities.
+///
+/// The algorithm used here is a nice compromise between simplicity, speed,
+/// and a small number of rectangle colliders.
+/// In basic terms, it will:
+/// 1. consider where the walls are
+/// 2. combine wall tiles into flat "plates" in each individual row
+/// 3. combine the plates into rectangles across multiple rows wherever possible
+/// 4. spawn colliders for each rectangle
+///
+/// Source: https://github.com/Trouv/bevy_ecs_ldtk/blob/201d908ae3e4f3deeb40de228f234c414c6b3141/examples/platformer/systems.rs#L62-L229
+///
+/// Modified roosta<mail@roosta.sh>
+/// Modified to use my own collision system instead of using rapier
 fn spawn_collisions(
     mut commands: Commands,
     wall_query: Query<(&GridCoords, &Parent), Added<Wall>>,
