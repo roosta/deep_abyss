0393b3a9bc059271a86204aa2be268d91986e0c4
Daniel Berg <mail@roosta.sh>
2024-02-16 15:50:08 +0100

    fix: clamp aspect ratio
    Fixes viewport aspect ratio for all possible widths.

--- src/camera.rs
+++ src/camera.rs
@@ -1,47 +1,57 @@
 use bevy::prelude::*;
 
-use bevy::render::camera::ScalingMode;
+use bevy::render::camera::{
+    ScalingMode,
+    Viewport,
+};
 
 #[derive(Component)]
 struct GameViewport;
 
 pub struct CameraPlugin;
 
+const ASPECT_RATIO: f32 = 16. / 9.;
+
 use crate::player::Player;
 
 fn setup(mut commands: Commands) {
-    let mut camera = Camera2dBundle {
-        camera: Camera {
-            order: 2,
-            ..default()
-        },
-        ..default()
+    let mut camera_game = Camera2dBundle::default();
+    camera_game.projection.scaling_mode = ScalingMode::AutoMax {
+        max_width: 384.0,
+        max_height: 432.0
     };
-    camera.projection.scaling_mode = ScalingMode::AutoMax {
-        max_width: 768.0,
-        max_height: 432.0,
-    };
-    commands.spawn((camera, GameViewport));
+    commands.spawn((camera_game, GameViewport));
 }
 
-fn clamp_camera(
-    mut camera_query: Query<&mut Transform, (With<GameViewport>, Without<Player>)>,
+fn clamp_viewport(
+    mut camera_query: Query<(&mut Transform, &mut Camera), (With<GameViewport>, Without<Player>)>,
     player_query: Query<&Transform, With<Player>>,
 ) {
     if let Ok(Transform {
         translation: player_translation,
         ..
-    }) = player_query.get_single()
-    {
-        let mut camera_transform = camera_query.single_mut();
+    }) = player_query.get_single() {
+        let (mut camera_transform, mut camera) = camera_query.single_mut();
         camera_transform.translation.x = player_translation.x;
         camera_transform.translation.y = player_translation.y;
+        if let Some(size) = camera.physical_target_size() {
+            let size_x = size.x as f32;
+            let size_y = size.y as f32;
+            let max_width = size_y / ASPECT_RATIO;
+            let center_x = (size_x / 2.) - (max_width / 2.);
+            camera.viewport = Some(Viewport {
+                physical_position: UVec2::new(center_x as u32, 0),
+                physical_size: UVec2::new(max_width as u32, size.y),
+                ..default()
+
+            })
+        }
     }
 }
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
-        app.add_systems(Update, clamp_camera);
+        app.add_systems(Update, clamp_viewport);
     }
 }
