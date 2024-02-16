0d2d80f583725f589c9ace3d9ccfe784eba420e1
Daniel Berg <mail@roosta.sh>
2024-02-16 15:48:39 +0100

    feat: center camera on player

--- src/camera.rs
+++ src/camera.rs
@@ -7,6 +7,8 @@ struct GameViewport;
 
 pub struct CameraPlugin;
 
+use crate::player::Player;
+
 fn setup(mut commands: Commands) {
     let mut camera = Camera2dBundle {
         camera: Camera {
@@ -22,8 +24,24 @@ fn setup(mut commands: Commands) {
     commands.spawn((camera, GameViewport));
 }
 
+fn clamp_camera(
+    mut camera_query: Query<&mut Transform, (With<GameViewport>, Without<Player>)>,
+    player_query: Query<&Transform, With<Player>>,
+) {
+    if let Ok(Transform {
+        translation: player_translation,
+        ..
+    }) = player_query.get_single()
+    {
+        let mut camera_transform = camera_query.single_mut();
+        camera_transform.translation.x = player_translation.x;
+        camera_transform.translation.y = player_translation.y;
+    }
+}
+
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
+        app.add_systems(Update, clamp_camera);
     }
 }
