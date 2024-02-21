0cb68e9bcd67b92557a33a61e7a03a600d3b34c9
Daniel Berg <mail@roosta.sh>
2024-02-21 21:09:55 +0100

    refactor: move player related camera its own fn

--- src/camera.rs
+++ src/camera.rs
@@ -23,35 +23,43 @@ fn setup(mut commands: Commands) {
     commands.spawn((camera_game, GameViewport));
 }
 
-fn clamp_viewport(
-    mut camera_query: Query<(&mut Transform, &mut Camera), (With<GameViewport>, Without<Player>)>,
+fn follow_player(
+    mut camera_query: Query<&mut Transform, (With<GameViewport>, Without<Player>)>,
     player_query: Query<&Transform, With<Player>>,
 ) {
+
     if let Ok(Transform {
         translation: player_translation,
         ..
     }) = player_query.get_single() {
-        let (mut camera_transform, mut camera) = camera_query.single_mut();
+        let mut camera_transform = camera_query.single_mut();
         camera_transform.translation.x = player_translation.x;
         camera_transform.translation.y = player_translation.y;
-        if let Some(size) = camera.physical_target_size() {
-            let size_x = size.x as f32;
-            let size_y = size.y as f32;
-            let max_width = size_y / ASPECT_RATIO;
-            let center_x = (size_x / 2.) - (max_width / 2.);
-            camera.viewport = Some(Viewport {
-                physical_position: UVec2::new(center_x as u32, 0),
-                physical_size: UVec2::new(max_width as u32, size.y),
-                ..default()
 
-            })
-        }
+    }
+}
+
+fn clamp_viewport(
+    mut camera_query: Query<&mut Camera, (With<GameViewport>, Without<Player>)>,
+    ) {
+    let mut camera = camera_query.single_mut();
+    if let Some(size) = camera.physical_target_size() {
+        let size_x = size.x as f32;
+        let size_y = size.y as f32;
+        let max_width = size_y / ASPECT_RATIO;
+        let center_x = (size_x / 2.) - (max_width / 2.);
+        camera.viewport = Some(Viewport {
+            physical_position: UVec2::new(center_x as u32, 0),
+            physical_size: UVec2::new(max_width as u32, size.y),
+            ..default()
+
+        })
     }
 }
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
-        app.add_systems(Update, clamp_viewport);
+        app.add_systems(Update, (clamp_viewport, follow_player));
     }
 }
