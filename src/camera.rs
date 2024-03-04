59b43eff9ce1a7bf3ae2ec1663087cff18aed2cb
Daniel Berg <mail@roosta.sh>
2024-03-04 19:02:03 +0100

    feat: clamp camera to level edges
    This will probably need work in the future, especially looking at level
    transform. Not sure how this will scale either, but it works like
    expected for now.

--- src/camera.rs
+++ src/camera.rs
@@ -1,14 +1,14 @@
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 use bevy::render::camera::{
+    OrthographicProjection,
     ScalingMode,
     Viewport,
 };
 
 #[derive(Component, Default)]
 struct GameViewport;
-
-
 
 #[derive(Default, Bundle)]
 pub struct CameraBundle {
@@ -19,7 +19,6 @@ pub struct CameraPlugin;
 
 const ASPECT_RATIO: f32 = 16. / 9.;
 const MAX_WIDTH: f32 = 384.0;
-const LEVEL_WIDTH: f32 = 768.0;
 const MAX_HEIGHT: f32 = 432.0;
 
 use crate::player::Player;
@@ -36,6 +35,7 @@ fn setup(mut commands: Commands) {
     });
 }
 
+/// Set camera translation based on player pos
 fn follow_player(
     mut camera_query: Query<&mut Transform, (With<GameViewport>, Without<Player>)>,
     player_query: Query<&Transform, With<Player>>,
@@ -52,8 +52,45 @@ fn follow_player(
     }
 }
 
+fn clamp_level(
+    level_query: Query<(&Transform, &LevelIid), (Without<GameViewport>, Without<Player>)>,
+    ldtk_projects: Query<&Handle<LdtkProject>>,
+    level_selection: Res<LevelSelection>,
+    ldtk_project_assets: Res<Assets<LdtkProject>>,
+    mut camera_query: Query<(
+        &mut Transform,
+        &OrthographicProjection,
+    ), (With<GameViewport>, Without<Player>)>,
+    ) {
+
+
+    let (mut camera_transform, projection) = camera_query.single_mut();
+    for (_level_transform, level_iid) in &level_query {
+        let ldtk_project = ldtk_project_assets
+            .get(ldtk_projects.single())
+            .expect("Project should be loaded if level has spawned");
+
+        let level = ldtk_project
+            .get_raw_level_by_iid(&level_iid.to_string())
+            .expect("Spawned level should exist in LDtk project");
+        if level_selection.is_match(&LevelIndices::default(), level) {
+
+            let min = projection.area.max.x;
+            let max = level.px_wid as f32 - projection.area.max.x;
+            camera_transform.translation.x = camera_transform.translation.x.clamp(min, max);
+
+            let max = level.px_hei as f32 - projection.area.max.y;
+            let min = projection.area.max.y;
+            camera_transform.translation.y = camera_transform.translation.y.clamp(min, max);
+        }
+    }
+}
+
+/// Set viewport size and position based on camera size (window)
+/// Ensures that the viewport takes as much space as possible
+/// TODO: Run only on window resize
 fn clamp_viewport(
-    mut camera_query: Query<&mut Camera, (With<GameViewport>, Without<Player>)>,
+    mut camera_query: Query<&mut Camera, With<GameViewport>>,
     ) {
     let mut camera = camera_query.single_mut();
     if let Some(size) = camera.physical_target_size() {
@@ -73,6 +110,10 @@ fn clamp_viewport(
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
-        app.add_systems(Update, (clamp_viewport, follow_player));
+        app.add_systems(Update, clamp_viewport);
+        app.add_systems(Update, (
+                follow_player,
+                clamp_level
+        ).chain());
     }
 }
