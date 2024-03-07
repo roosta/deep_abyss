35f6a2bef2043fcef59741e4258a702d3e02f58a
Daniel Berg <mail@roosta.sh>
2024-03-07 18:03:44 +0100

    chore(fmt): format camera.rs

--- src/camera.rs
+++ src/camera.rs
@@ -1,11 +1,7 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
-use bevy::render::camera::{
-    OrthographicProjection,
-    ScalingMode,
-    Viewport,
-};
+use bevy::render::camrra::{OrthographicProjection, ScalingMode, Viewport};
 
 #[derive(Component, Default)]
 struct GameViewport;
@@ -27,7 +23,7 @@ fn setup(mut commands: Commands) {
     let mut camera_game = Camera2dBundle::default();
     camera_game.projection.scaling_mode = ScalingMode::AutoMax {
         max_width: MAX_WIDTH,
-        max_height: MAX_HEIGHT
+        max_height: MAX_HEIGHT,
     };
     commands.spawn(CameraBundle {
         camera_bundle: camera_game,
@@ -40,15 +36,14 @@ fn follow_player(
     mut camera_query: Query<&mut Transform, (With<GameViewport>, Without<Player>)>,
     player_query: Query<&Transform, With<Player>>,
 ) {
-
     if let Ok(Transform {
         translation: player_translation,
         ..
-    }) = player_query.get_single() {
+    }) = player_query.get_single()
+    {
         let mut camera_transform = camera_query.single_mut();
         camera_transform.translation.x = player_translation.x;
         camera_transform.translation.y = player_translation.y;
-
     }
 }
 
@@ -57,13 +52,11 @@ fn clamp_level(
     ldtk_projects: Query<&Handle<LdtkProject>>,
     level_selection: Res<LevelSelection>,
     ldtk_project_assets: Res<Assets<LdtkProject>>,
-    mut camera_query: Query<(
-        &mut Transform,
-        &OrthographicProjection,
-    ), (With<GameViewport>, Without<Player>)>,
-    ) {
-
-
+    mut camera_query: Query<
+        (&mut Transform, &OrthographicProjection),
+        (With<GameViewport>, Without<Player>),
+    >,
+) {
     let (mut camera_transform, projection) = camera_query.single_mut();
     for (_level_transform, level_iid) in &level_query {
         let ldtk_project = ldtk_project_assets
@@ -74,7 +67,6 @@ fn clamp_level(
             .get_raw_level_by_iid(&level_iid.to_string())
             .expect("Spawned level should exist in LDtk project");
         if level_selection.is_match(&LevelIndices::default(), level) {
-
             let min = projection.area.max.x;
             let max = level.px_wid as f32 - projection.area.max.x;
             camera_transform.translation.x = camera_transform.translation.x.clamp(min, max);
@@ -89,9 +81,7 @@ fn clamp_level(
 /// Set viewport size and position based on camera size (window)
 /// Ensures that the viewport takes as much space as possible
 /// TODO: Run only on window resize
-fn clamp_viewport(
-    mut camera_query: Query<&mut Camera, With<GameViewport>>,
-    ) {
+fn clamp_viewport(mut camera_query: Query<&mut Camera, With<GameViewport>>) {
     let mut camera = camera_query.single_mut();
     if let Some(size) = camera.physical_target_size() {
         let size_x = size.x as f32;
@@ -102,7 +92,6 @@ fn clamp_viewport(
             physical_position: UVec2::new(center_x as u32, 0),
             physical_size: UVec2::new(max_width as u32, size.y),
             ..default()
-
         })
     }
 }
@@ -111,9 +100,6 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
         app.add_systems(Update, clamp_viewport);
-        app.add_systems(Update, (
-                follow_player,
-                clamp_level
-        ).chain());
+        app.add_systems(Update, (follow_player, clamp_level).chain());
     }
 }
